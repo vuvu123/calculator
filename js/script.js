49659b02f62f492b8b5ba666b25774661f53a0d1
Kenny <vuvu123@gmail.com>
2022-09-01 14:06:13 -0700

    Refactor operate function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
+const displayValue = document.querySelector("#currentOperationScreen");
+
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
-let operate = (a, b, operator) => {
-  if (operator === 'add') {
-    return add(a, b);
-  } else if (operator === 'subtract') {
-    return subtract(a, b);
-  } else if (operator === 'multiply') {
-    return multiply(a, b);
-  } else {
-    return divide(a, b);
+function operate(a, b, operator) {
+  a = parseInt(a);
+  b = parseInt(b);
+
+  switch(operator) {
+    case 'x':
+      return multiply(a, b);
+    case '÷':
+      if (b === 0) return null
+      return divide(a, b);
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    default:
+      return null;
   }
 };
 
+function populateDisplay(input) {
+  displayValue.textContent += input;
+};
