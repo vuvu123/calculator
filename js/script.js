3df418656fa181098a4f98f70a21aefe2c6aff3e
Kenny <vuvu123@gmail.com>
2022-09-08 13:57:45 -0700

    Append numbers to screen on number button click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-const displayValue = document.querySelector("#currentOperationScreen");
+// DOM
+const numberButtons = document.querySelectorAll(".number-btn");
+const equalsButton = document.getElementById("equals");
+const decimalButton = document.getElementById("decimal");
+const divideButton = document.getElementById("divide");
+const multiplyButton = document.getElementById("multiply");
+const subtractButton = document.getElementById("subtract");
+const addButton = document.getElementById("add");
+const currentScreen = document.getElementById("currentOperationScreen");
+const lastScreen = document.getElementById("lastOperationScreen");
+
+function appendDisplay(input) {
+  currentScreen.textContent += input;
+};
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => appendDisplay(button.textContent));
+});
 
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
@@ -24,6 +41,3 @@ function operate(a, b, operator) {
   }
 };
 
-function populateDisplay(input) {
-  displayValue.textContent += input;
-};
